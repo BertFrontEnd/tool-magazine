b2bbca7d4889cd3d4e060c9a9f6ec3dbe2256e50
BertFrontEnd <Berton@yandex.ru>
2022-06-29 16:26:54 +0300

    feat: add structure of header

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const callElement = document.querySelector('.call-button');
-const closeElement = document.querySelector('.element__close');
-
-callElement.addEventListener('click', () => {
-  document.querySelector('.pop-up').classList.remove('inactive');
-});
-
-closeElement.addEventListener('click', () => {
-  document.querySelector('.pop-up').classList.add('inactive');
-});
+console.log('tool-magazine');
